2443d2015b48256b2395a89c2c34696174e61793
ASIMNEUPANE <asimneupane11@gmail.com>
2024-06-23 16:02:33 +0545

    generateHas

---
+++ 2day.js
@@ -0,0 +1,19 @@
+const generateHash = (str) => {
+  if (str.trim().length === 0 || str.length > 200) {
+    return false;
+  }
+
+  str = str.trim().split(' ');
+
+  str = str.map((strs) => {
+    // return strs.replace(strs[0], strs[0].toUpperCase());
+    return strs.charAt(0).toUpperCase() + strs.slice(1);
+  });
+
+  let result = str.join(''); // Join the array into a single string
+  let resultArray = result.split(''); // Convert the string into an array of characters
+  resultArray.unshift('#'); // Add '#' as the first element in the array
+
+  return resultArray.join('');
+};
+console.log(generateHash('hy name is lakhan '));
