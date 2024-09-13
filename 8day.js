af7c38d5e3811d6d26d668bccaa7fb4fef0869e3
asim <asimneupane11@gmail.com>
2024-09-13 12:50:14 +0545

    day8

---
+++ 8day.js
@@ -0,0 +1,33 @@
+// const factorial = (num) => {
+//     if(num<0){
+//         console.log("Factorial is not defined for negative numbers.");
+//     }
+//     if (num === 0) return 1;
+//     if (num === 1) return 1;
+  
+//     let val = 1;
+  
+//     for (let i = num; i > 0; i--) {
+//       val *= i; 
+//     }
+  
+//     return val; 
+//   };
+
+const factorial = (num) => {
+    // Check if the number is negative
+    if (num < 0) {
+      console.log("Factorial is not defined for negative numbers.");
+      return;
+    }
+  
+    // Handle base cases for 0 and 1
+    if (num === 0 || num === 1) return 1;
+  
+    // Recursive call to calculate factorial
+    return num * factorial(num - 1);
+  };
+  
+  console.log(factorial(3));
+// factorial(-2)
+  
