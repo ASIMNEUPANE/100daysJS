e3848c2873759b8eedca0a1b6bc00027ca3ab964
asim <asimneupane11@gmail.com>
2024-11-11 23:57:01 +0545

    day19

---
+++ 19.js
@@ -0,0 +1,10 @@
+const check=(str,substr)=>{
+    // 1st method
+    // return str.startsWith(substr)
+    // 2nd
+//    return str.split(" ")[0] == "hello" ?  true : false
+    
+
+}
+
+console.log(check("hello        i am asim","hello"))
